e9aa27b118593e6f600ff97dc773e30e51a0691e
Warheart-1 <axel97477@gmail.com>
2022-05-14 13:11:17 +0200

    rajout de l'appel a l'api mal + rajout d'axios

--- commands/add.js
+++ commands/add.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MALClientId } = require('../config/config.json');
+const axios = require('axios');
 const fs = require('node:fs');    
 
 // création d'une commande
@@ -11,8 +13,29 @@ module.exports = {
                 .setDescription('Le nom de l\'anime à ajouter.')
                 .setRequired(true)
         ),
+
+    async VerifyAnimeName(name){
+        const anime = name;
+        const config = {
+            headers: {
+                "X-MAL-CLIENT-ID" : MALClientId,
+            }
+        }
+        const url = `https://api.myanimelist.net/v2/anime?q=${anime}`
+        await axios.get(url, config)
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        }
+        )
+    },
+
     async execute(interaction) {
         const anime = interaction.options.getString('anime'); // récupère l'option de la commande, ici un string (qui a été définie plus haut)
+        this.VerifyAnimeName(anime);
         const file = fs.readFileSync('./test/test.json');
         const animes = JSON.parse(file);
         animes.name.push(anime);
@@ -20,4 +43,5 @@ module.exports = {
         await interaction.reply(`${anime} a été ajouté à la liste.`);       
         console.info("ce que contient le fichier test.json : " + animes.name)   
     }
-}
+
+} 
